77cb6edfe6a0959d2482f761584041ba5538f985
bhowmikp <prantarbhowmik@yahoo.com>
2016-06-03 20:30:52 -0400

    get website to send message

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,12 @@ function onClick() {
 	document.getElementById("clicks").innerHTML = clicks;
 };
 
-// Connect with extension
-chrome.runtime.sendMessage('mhklkaddlabkplhnjdnimihdpofbmecp', 'dummymessage')
+// The ID of the extension we want to talk to.
+var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+// Make a simple request:
+chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
+  function(response) {
+    if (!response.success)
+      handleError(url);
+  });
