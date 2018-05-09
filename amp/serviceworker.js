7d00ee0f02e06120c1aac2877f22dc8b4e8b7546
Prantar <prantarbhowmik@yahoo.com>
2018-05-09 02:10:16 -0400

    Service worker work offline

--- amp/serviceworker.js
+++ amp/serviceworker.js
@@ -1,13 +1,42 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      console.log("Created serviceworker");
-      return cache.addAll([
-        '/favicon.ico',
-        '/assets/background.jpeg',
-        '/assets/Prantar-Resume.pdf',
-        '/assets/prantar.png'
-      ]);
+var cacheName = 'myCacheVersion';
+var filesToCache = [
+  '/favicon.ico',
+  '/assets/background.jpeg',
+  '/assets/Prantar-Resume.pdf',
+  '/assets/prantar.png'
+];
+
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
