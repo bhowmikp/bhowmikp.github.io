4ed03ad4a74e0845b4eca73b3a82a727b1a142c4
Prantar <prantarbhowmik@yahoo.com>
2019-02-26 00:02:26 -0500

    Make sortable recognition

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RestApiService } from '../service/rest-api.service';
 import { Observable } from 'rxjs';
+import {Sort} from '@angular/material';
+
+export interface Recognition {
+  name: string;
+  place: string;
+  year: number;
+}
 
 @Component({
   selector: 'app-experience',
@@ -11,9 +18,36 @@ export class ExperienceComponent implements OnInit {
 
   recognitionItems: Observable<any[]>;
 
+  sortedData: any;
+
   constructor(private restApiService: RestApiService) { }
 
   ngOnInit() {
-    this.recognitionItems = this.restApiService.getRecognition();
+    this.restApiService.getRecognition().subscribe(data => {
+      console.log(data.slice());
+      this.sortedData = data.slice();
+    });
   }
+
+  sortData(sort: Sort) {
+    const data = this.sortedData.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'place': return compare(a.place, b.place, isAsc);
+        case 'year': return compare(a.year, b.year, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
