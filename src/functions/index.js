9b738b78e426e45ee2e59f977479261491380ba7
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2020-06-27 20:01:38 -0400

    #8 Set up amp

--- src/functions/index.js
+++ src/functions/index.js
@@ -3,7 +3,8 @@
 const functions = require("firebase-functions");
 const next = require("next");
 
-const app = next({ dev: false, conf: { distDir: "next" } });
+const dir = __dirname + "/app";
+const app = next({ dev: false, dir, conf: { distDir: "next" } });
 const handle = app.getRequestHandler();
 
 exports.next = functions.https.onRequest(async (req, res) => {
