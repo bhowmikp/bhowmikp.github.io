2dee7d8aec580a5784bfd2b628ad9a713d27e958
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2020-06-27 20:35:26 -0400

    #8 set up amp (#12)
    * #8 set install script
    
    * #8 Set up amp
    
    * #8 Fix deploy script
    
    * #8 Updated packages

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,7 +1,15 @@
 import App from "../components/App";
+import { useAmp } from "next/amp";
 
-export default () => (
-  <App>
-    <p>Index Page</p>
-  </App>
-);
+export const config = { amp: "hybrid" };
+
+export default () => {
+  const isAmp = useAmp();
+
+  return (
+    <App>
+      <p>Index Page</p>
+      {isAmp ? <p>Amp site</p> : <p>Regular site</p>}
+    </App>
+  );
+};
