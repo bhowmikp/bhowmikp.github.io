19ae677adccae255e47b77b2af7c0fa822967290
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2020-07-04 03:33:37 -0400

    #15 Get all data from database

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,7 +1,6 @@
 import { FirestoreService } from "../../service/FirestoreService";
-import { FirestoreServiceModel } from "../../service/FirestoreService/firestoreService";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(await FirestoreService.getExperience());
+  res.status(200).json(await FirestoreService.getProjects());
 };
---
+++ src/service/FirestoreService/firestoreService.d.ts
@@ -0,0 +1,32 @@
+export declare namespace FirestoreServiceModel {
+  interface IExperience {
+    title: string;
+    end: {
+      seconds: number;
+      nanoseconds: number;
+    };
+    start: {
+      seconds: number;
+      nanoseconds: number;
+    };
+    time: string;
+    abbr: string;
+    employer: string;
+    responsibility: string[];
+  }
+
+  interface IProjects {
+    title: string;
+    order: number;
+    features: string[];
+    name: string;
+    links: any[];
+  }
+
+  interface IRecognition {
+    title: string;
+    place: string;
+    year: string;
+    name: string;
+  }
+}
