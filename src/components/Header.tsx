bcb65238d3846da466d2ea73ae412894d05c81cd
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2020-07-04 22:02:43 -0400

    #19 Added experience page (#21)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,35 @@
 import * as React from "react";
 import Link from "next/link";
+import { useAmp } from "next/amp";
 
-const Header = () => (
-  <header>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </header>
-);
+export const config = { amp: "hybrid" };
+
+const Header = () => {
+  const isAmp = useAmp();
+
+  return (
+    <header>
+      {isAmp ? (
+        <Link href="/index?amp=1">
+          <a>Home</a>
+        </Link>
+      ) : (
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      )}
+
+      {isAmp ? (
+        <Link href="/experience?amp=1">
+          <a>Experience</a>
+        </Link>
+      ) : (
+        <Link href="/experience">
+          <a>Experience</a>
+        </Link>
+      )}
+    </header>
+  );
+};
 
 export default Header;
--- src/service/FireStorageService/index.ts
+++ src/service/FireStorageService/index.ts
@@ -1,7 +1,11 @@
 require("dotenv").config();
 
 export const FireStorageService = {
-  getProfilePciture: async (): Promise<any> => {
+  getProfilePciture: async (): Promise<string> => {
     return `https://firebasestorage.googleapis.com/v0/b/${process.env.FIREBASE_STORAGE_BUCKET}/o/profilePicture.jpg?alt=media`;
+  },
+
+  getResume: async (): Promise<string> => {
+    return `https://firebasestorage.googleapis.com/v0/b/${process.env.FIREBASE_STORAGE_BUCKET}/o/Prantar-Resume.pdf?alt=media`;
   }
 };
