4dd52a51936634fd5304ab38a63414a74b91041c
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2021-02-27 03:26:47 -0500

    fix(css): added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import Head from 'next/head'
 import AppLayout from '@Components/AppLayout';
+import { useState } from 'react';
 
 const Home = () => {
+
+  const [value, setValue] = useState(0);
+
   return (
     <AppLayout title="Homepage">
-      <p>Test</p>
+
+      <div className="h-20 flex items-center justify-center">
+        <button className={`px-10 py-2 bg-red-500 text-white rounded-md ${value === 0 ? 'animate-bounce' : ''}`} onClick={() => { setValue(value + 1) }}>
+          Click Me
+      </button>
+      </div>
+      <p>{value}</p>
     </AppLayout>
   )
 }
