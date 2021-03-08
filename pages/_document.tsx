eed2eba9b2f61dfa332afaa8275a5c3b215b879e
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-03-08 05:33:32 -0500

    feat(google): added ga, gtm, adsense integration (#36)
    * feat(google): added ga, gtm, adsense integration
    
    * feat(google): added amp integration for ads
    
    * feat(google): fixed amp integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,75 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import React from 'react';
+import { useAmp } from 'next/amp';
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx): Promise<any> {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+import { GA_TRACKING_ID } from '@Service/googleService';
+import AmpAnalytics from '@Components/AmpAnalytics';
+import React, { ReactElement } from 'react';
 
-    render(): JSX.Element {
+const AmpWrap = ({ ampOnly, nonAmp }: { ampOnly?: ReactElement; nonAmp?: ReactElement }) => {
+    const isAmp = useAmp();
+    if (ampOnly) return isAmp && ampOnly;
+    return !isAmp && nonAmp;
+};
+
+export default class MyDocument extends Document {
+    render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
+
+                    {/* AMP - Google Analytics */}
+                    <AmpWrap
+                        ampOnly={
+                            <>
+                                <AmpAnalytics
+                                    type="googleanalytics"
+                                    script={{
+                                        vars: {
+                                            account: GA_TRACKING_ID,
+                                            gtag_id: GA_TRACKING_ID,
+                                            config: {
+                                                [GA_TRACKING_ID]: { groups: 'default' }
+                                            }
+                                        },
+                                        triggers: {
+                                            trackPageview: {
+                                                on: 'visible',
+                                                request: 'pageview'
+                                            }
+                                        }
+                                    }}
+                                />
+                            </>
+                        }
+                    />
+
+                    {/* Non-AMP - Google Analytics */}
+                    <AmpWrap
+                        nonAmp={
+                            <>
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+                                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                                <script
+                                    dangerouslySetInnerHTML={{
+                                        __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', '${GA_TRACKING_ID}');
+                    `
+                                    }}
+                                />
+                            </>
+                        }
+                    />
                 </body>
             </Html>
         );
     }
 }
-
-export default MyDocument;
