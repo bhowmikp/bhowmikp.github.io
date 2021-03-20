85c4efceb9befe5e41f9227e230a42720a3dd264
Prantar Bhowmik <prantar.bhowmik@rbc.com>
2021-03-20 17:41:09 -0400

    feat(recognition): change styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './serializers/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class', // or 'media' or 'class'
-    important: true,
     theme: {
         extend: {
             backgroundColor: {
