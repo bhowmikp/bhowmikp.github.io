82791e7b965f651c40481be4a019a3e1cf235cf6
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-11-20 16:07:56 -0500

    feat(about): added about me page (#91)
    * feat(about): added about me page
    
    * fix(css): added some classes to tailwind safelist

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,41 +15,39 @@ const PageNotFound: FC & { getLayout: ReactNode } = () => {
     const { resolvedTheme } = useTheme();
 
     return (
-        <>
-            <div className="flex py-10">
-                <div className="m-auto">
-                    <p className="text-center mb-10 text-3xl font-bold my-2 text-black dark:text-white">
-                        Oops! Page not found
-                    </p>
-                    {isAmp ? (
-                        <a href="https://www.vecteezy.com/free-vector/cute" rel="noopener noreferrer">
-                            <amp-img width="600" height="600" src="/hangInThereLight.svg" alt="404 Image" />
-                        </a>
-                    ) : (
-                        <Image
-                            src={resolvedTheme === 'light' ? '/hangInThereLight.svg' : '/hangInThereDark.svg'}
-                            alt="404 Image"
-                            width={500}
-                            height={500}
-                            onClick={() => {
-                                window.open('https://www.vecteezy.com/free-vector/cute', '_ blank');
-                            }}
-                        />
-                    )}
-                    <p className="text-center mt-10 text-xl font-bold my-2 text-black dark:text-white">
-                        Try{' '}
-                        <Link href="/">
-                            <a className="link-primary">homepage</a>
-                        </Link>{' '}
-                        or{' '}
-                        <Link href="/blog">
-                            <a className="link-primary">blog</a>
-                        </Link>{' '}
-                        instead
-                    </p>
-                </div>
+        <div className="flex py-10">
+            <div className="m-auto">
+                <p className="text-center mb-10 text-3xl font-bold my-2 text-black dark:text-white">
+                    Oops! Page not found
+                </p>
+                {isAmp ? (
+                    <a href="https://www.vecteezy.com/free-vector/cute" rel="noopener noreferrer">
+                        <amp-img width="600" height="600" src="/hangInThereLight.svg" alt="404 Image" />
+                    </a>
+                ) : (
+                    <Image
+                        src={resolvedTheme === 'light' ? '/hangInThereLight.svg' : '/hangInThereDark.svg'}
+                        alt="404 Image"
+                        width={500}
+                        height={500}
+                        onClick={() => {
+                            window.open('https://www.vecteezy.com/free-vector/cute', '_ blank');
+                        }}
+                    />
+                )}
+                <p className="text-center mt-10 text-xl font-bold my-2 text-black dark:text-white">
+                    Try{' '}
+                    <Link href="/">
+                        <a className="link-primary">homepage</a>
+                    </Link>{' '}
+                    or{' '}
+                    <Link href="/blog">
+                        <a className="link-primary">blog</a>
+                    </Link>{' '}
+                    instead
+                </p>
             </div>
-        </>
+        </div>
     );
 };
 
---
+++ pages/about.tsx
@@ -0,0 +1,56 @@
+import React, { FC } from 'react';
+import { GetStaticProps } from 'next';
+
+import type { ReactNode, ReactElement } from 'react';
+
+import BlockContent from '@sanity/block-content-to-react';
+import { figureSerializer } from '@Sanity/serializers/figure';
+
+import { AppLayout } from '@Components/AppLayout';
+import { PageCover } from '@Components/Common/PageCover';
+
+import { getPageData } from '@Api/page/[page]';
+import { IAboutMeData } from '@Interfaces/pages/aboutMePage';
+
+import { time as timeConstants } from '@Constants';
+
+export const getStaticProps: GetStaticProps = async () => ({
+    props: {
+        aboutMeData: await getPageData('aboutMe')
+    },
+    revalidate: timeConstants.oneDayInSeconds
+});
+
+const serializers = {
+    types: {
+        block: (props) => <p className="text-secondary text-xl">{props.children}</p>
+    }
+};
+
+const AboutMePage: FC<{ aboutMeData: IAboutMeData }> & { getLayout: ReactNode } = ({ aboutMeData }) => (
+    <>
+        <PageCover pageCoverData={aboutMeData.heading} />
+
+        <div className="bg-secondary py-10">
+            <div className="mx-auto px-3 py-2 md:px-0 md:py-0 md:w-8/12 lg:px-14">
+                <div className="w-11/12 xl:w-10/12 mx-auto">
+                    <p className="font-medium text-4xl mb-5">{aboutMeData.paragraphTitle}</p>
+
+                    <div className={`w-4/12 float-${aboutMeData.profilePic.alignment} mx-5`}>
+                        <BlockContent blocks={aboutMeData.profilePic} serializers={figureSerializer} />
+                    </div>
+
+                    <BlockContent blocks={aboutMeData.paragraphDescription} serializers={serializers} />
+                </div>
+            </div>
+        </div>
+    </>
+);
+
+AboutMePage.getLayout = (page: ReactElement) => (
+    <AppLayout title="About Me" mainClassName="bg-primary">
+        {page}
+    </AppLayout>
+);
+
+export default AboutMePage;
