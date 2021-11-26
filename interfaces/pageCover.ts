3c8c8893aa520b308c615fa7883c304e9bd32d5f
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-11-26 18:56:42 -0500

    feat(stepper): added stepper to page cover

--- interfaces/pageCover.ts
+++ interfaces/pageCover.ts
@@ -1,4 +1,11 @@
+export interface IPageCoverStepper {
+    stepperLabel: string;
+    stepperLink: string;
+    _key: string;
+}
+
 export interface IPageCover {
     header: string;
     description: string;
+    pageStepper?: IPageCoverStepper[];
 }
