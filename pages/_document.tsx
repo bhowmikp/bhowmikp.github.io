2ad0b4b02ed122372cc79f5f6c6b67779691dacc
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-12-12 23:21:55 -0500

    feat(ads): added script for google ads (#109)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-import { GA_TRACKING_ID } from '@Service/googleService';
 import React from 'react';
 
 export default class MyDocument extends Document {
@@ -13,8 +11,15 @@ export default class MyDocument extends Document {
                     <Main />
                     <NextScript />
 
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
-                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                    <script
+                        data-ad-client={`${process.env.NEXT_PUBLIC_AD_CLIENT}`}
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    />
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                    />
                     <script
                         // eslint-disable-next-line react/no-danger
                         dangerouslySetInnerHTML={{
@@ -23,7 +28,7 @@ export default class MyDocument extends Document {
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
 
-                                gtag('config', '${GA_TRACKING_ID}');
+                                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
                                 `
                         }}
                     />
