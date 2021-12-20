72a7946f85d8038fd2e7a655e2ff0a1550533ba7
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2021-12-20 16:14:50 -0500

    feat(ad): please disable adblock

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -18,16 +18,22 @@ export const AdBanner: FC = () => {
     }, []);
 
     return (
-        <ins
-            className="adsbygoogle block text-center bg-secondary"
-            data-ad-layout="in-article"
-            data-ad-format="fluid"
-            data-ad-client="ca-pub-1306989929207512"
-            data-ad-slot={
-                resolvedTheme === 'light' ? process.env.NEXT_PUBLIC_AD_SLOT_LIGHT : process.env.NEXT_PUBLIC_AD_SLOT_DARK
-            }
-            data-adtest={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 'off' : 'on'}
-        />
+        <>
+            <p className="text-center relative top-10">Please disable adblock</p>
+
+            <ins
+                className="adsbygoogle block text-center bg-secondary"
+                data-ad-layout="in-article"
+                data-ad-format="fluid"
+                data-ad-client="ca-pub-1306989929207512"
+                data-ad-slot={
+                    resolvedTheme === 'light'
+                        ? process.env.NEXT_PUBLIC_AD_SLOT_LIGHT
+                        : process.env.NEXT_PUBLIC_AD_SLOT_DARK
+                }
+                data-adtest={process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 'off' : 'on'}
+            />
+        </>
     );
 };
 
